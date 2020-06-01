9bb18c1abdbf1dfe96d8f5ff3a036a2ccb48c541
Eduardo Rauta <ebrauta@gmail.com>
2020-06-01 03:23:01 -0300

    Diminuição do index e do script com retirada do jQuery e separação da constante com as moedas

---
+++ scripts/moedas.js
@@ -0,0 +1,58 @@
+const moedas = [
+  {
+    name: "Reais",
+    simb: "R$",
+    tax: 1,
+    country: "Brasil",
+    currency: "BRL",
+  },
+  {
+    name: "Dólares Americanos",
+    simb: "US$",
+    tax: 5.35,
+    country: "EUA",
+    currency: "USD",
+  },
+  {
+    name: "Euros",
+    simb: "€",
+    tax: 5.96,
+    country: "Europa",
+    currency: "EUR",
+  },
+  {
+    name: "Libras Esterlinas",
+    simb: "£",
+    tax: 6.63,
+    country: "Grã-Bretanha",
+    currency: "CGP",
+  },
+  {
+    name: "Ienes",
+    simb: "JP¥",
+    tax: 0.05,
+    country: "Japão",
+    currency: "JPY",
+  },
+  {
+    name: "Yuans",
+    simb: "CN¥",
+    tax: 0.75,
+    country: "China",
+    currency: "CNY",
+  },
+  {
+    name: "Rublos russos",
+    simb: "₽",
+    tax: 0.076,
+    country: "Rússia",
+    currency: "RUB",
+  },
+  {
+    name: "Peso Argentino",
+    simb: "$",
+    tax: 0.079,
+    country: "Argentina",
+    currency: "ARS",
+  }
+];
--- scripts/script.js
+++ scripts/script.js
@@ -1,106 +1,75 @@
-function convert() {
-  // Declare variables for input value, observation, options and message
-  let message;
-  let obs = $("#obs");
-  let inputnum = document.getElementById("number");
-  let num = inputnum.value;
-  let options = [$('#moeda1')[0].options.selectedIndex, $('#moeda2')[0].options.selectedIndex];
+var valor = document.getElementById("number");
+var moeda1 = document.getElementById("moeda1");
+var moeda2 = document.getElementById("moeda2");
+var obs = document.getElementById("obs");
+var total = document.getElementById("total");
 
-  // Changing , by . an verifing if num is null or empty
-  num = num.replace(",", ".");
-  num = num.replace(" ", "");
-  NumberNull(num);
-  // Delete previous result
-  remove();
+function criarSelect(select) {
+  let opcoes = moedas.map((item) => item.name);
+  opcoes.forEach((item, index) => {
+    let opcao = document.createElement("option");
+    opcao.setAttribute("value", index);
+    opcao.innerHTML = item;
+    select.appendChild(opcao);
+  });
+}
+criarSelect(moeda1);
+criarSelect(moeda2);
 
-  // Check input value
-  let textincorrect = "Este valor está incorreto!";
-  let erros = [
-    "Caracter inválido!!!",
-    "Insira apenas 2 casa após a vírgula!!!"
-  ];
-  if (!Number.isFinite(+num)) {
-    // Not's number
-    message = `<div class="total">${textincorrect} <br> ${erros[0]} </div>`;
-  } else if (num.indexOf(".") != -1 && num.indexOf(".") < num.length - 3) {
-    // More then 2 digits
-    message = `<div class="total">${textincorrect} <br> ${erros[1]} </div>`;
-  } else {
-    // Number -> 0 = real, 1 = dolar, 2 = euros, 3 = pounds(libras)
-    obs.text("* Taxa de 19/03/2020 - Atualizada mensalmente")
-    message = `<div class="total">`;
-    let vlr_moedas = RealToOther(num, options[0], options[1]);
-    message += `${vlr_moedas}`;
-    message += `</div>`;
-  }
-  // Create block for function's message
-  document.querySelector(".window").insertAdjacentHTML("beforeend", message);
-
-  //Transform from Real to Other
-  function RealToOther(number, money1, money2) {
-    NumberNull(number);
-    //Taxa de 20/03/2020 - real, dolar, euro, libra, iene, bitcoin
-    // Declare variables
-    let moedas = {
-      "name":[ "Reais", "Dólares americanos", "Euros", "Libras esterlinas", "Ienes","Yuans", "Rublos russos"],
-      "simb" : ["R$", "US$", "€", "£", "JP¥", "CN¥", "₽"],
-      "taxes" : [1, 5.00, 5.33, 5.82, 0.045, 0.70, 0.063]
-    }
-    // Convert both to Real
-    let tax1 = moedas.taxes[money1];
-    let value1 = number / tax1;
-    let tax2 = moedas.taxes[money2];
-    let value2 = number / tax2;
-    // Convert 
-    let value, saida = '';
-    if(value1 == 0 || value2 == 0){
-      value = 0
-      saida += `Valor em ${moedas.name[money1]}: ${moedas.simb[money1]} 0.00 <br>`
-      saida += `Valor em ${moedas.name[money2]}: ${moedas.simb[money2]} 0.00`
-    } else {
-      value = value2/value1
-      saida += `Valor em ${moedas.name[money1]}: ${moedas.simb[money1]} ${parseFloat(number).toFixed(2)} <br>`
-      saida += `Valor em ${moedas.name[money2]}: ${moedas.simb[money2]} ${value.toFixed(2)}`
-    }
-    return saida;
-  }
-  // Focus the input
-  inputnum.focus();
+function toNumbers(num, tax) {
+  return num * tax;
 }
 
-// Clean all
-function clean() {
-  // Declare variables for input value and options
-  let num = document.getElementById("number");
-  let options = [$('#moeda1')[0], $('#moeda2')[0]];
-
-  // Remove list
-  remove();
-
-  // Clean input value and receive focus
-  num.value = "";
-  num.focus();
-
-  //Clean options
-  options.forEach(e => {
-    e.selectedIndex = 0
+function transformCurrency(value, currency) {
+  return Number(value).toLocaleString("pt-br", {
+    style: "currency",
+    currency: currency,
   });
 }
 
-// Remove table
-function remove(){
-  // Declare variables for table and observation
-  let total = $(".total")
-  let obs = $("#obs")
-  // Clean values
-  if (total) {
-    total.remove();
-    obs.text("")
+function testaNumero(numero) {
+  total.style.display = "none"
+  if (!Number.isFinite(+numero)) {
+    let textincorrect = "Este valor está incorreto: Caracter inválido!";
+    let erro = "Por favor, insira um número!!!";
+    obs.innerHTML = `${textincorrect} <br> ${erro}`;
+  } else if (numero < 0) {
+    let textincorrect = "Este valor está incorreto! Número negativo!";
+    let erro = "Por favor, insira um número positivo!!!";
+    obs.innerHTML = `${textincorrect} <br> ${erro}`;
+  } else {
+    let option1 = moeda1.options.selectedIndex;
+    let opc1 = [moedas[option1].name,moedas[option1].country]
+    let option2 = moeda2.options.selectedIndex;
+    let opc2 = [moedas[option2].name,moedas[option2].country]
+    let tax = moedas[option1].tax / moedas[option2].tax;
+    let transformed = toNumbers(numero, tax);
+    let saida = `Valor em ${opc1[0]} (${opc1[1]}): ${transformCurrency(
+      numero,
+      moedas[option1].currency
+    )}<br>`;
+    saida += `Valor em ${opc2[0]} (${opc2[1]}): ${transformCurrency(
+      transformed,
+      moedas[option2].currency
+    )}`;
+    total.innerHTML = saida;
+    total.style.display = "block";
   }
 }
-// Verify if number is null ou empty
-function NumberNull(number){
-  if (number == null || number == "") {
-    number = 0;
-  } 
+
+function convert() {
+  let num = valor.value;
+  num = num.replace(",", ".");
+  num = num.replace(" ", "");
+  num = Number(num);
+  testaNumero(num);
+}
+
+function clean() {
+  valor.value = "";
+  moeda1.options.selectedIndex = 0;
+  moeda2.options.selectedIndex = 0;
+  obs.innerHTML = "";
+  total.innerHTML = "";
+  total.style.display = "none";
 }
